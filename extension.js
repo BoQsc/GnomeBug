8f103fbf85bcab188f0f59bb97e991210b4e36b6
Feldwor <BoQsc@users.noreply.github.com>
2018-10-17 18:14:28 +0000

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 //Importing Native Modules 
 const ExtensionUtils = imports.misc.extensionUtils;
 const ME = ExtensionUtils.getCurrentExtension();
-const panel = ME.imports.panelButton;
+const panel = ME.imports.PanelButton;
 const Main = imports.ui.main;
 
 
